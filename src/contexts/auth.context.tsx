00e8a79f7735ad7df9aa5e271c77c9031deb8ae5
Pedro Mendes <pedro.goncalves@bairesdev.com>
2022-02-09 20:57:32 -0300

    removing unused comment

--- src/contexts/auth.context.tsx
+++ src/contexts/auth.context.tsx
@@ -27,15 +27,6 @@ interface IProvider {
 export const AuthStateProvider = ({ children, reducer, initialState }: IProvider) => {
 	const [globalAuthState, authDispatch] = useReducer<Reducer<IAuth, any>>(reducer, initialState);
 
-	/**
-	 * Updates people state with given payload
-	 * @param {string} type should be taken from the peopleReducer
-	 * @param {string} id of the person
-	 * @param {IPeople} payload should be one field from IPeople
-	 * @param {string} videoId pass this only when you want to remove a video
-	 * @param {string} documentId pass this only when you want to remove a document
-	 * @return {void} Dispatch the action using reducers
-	 */
 	const updateAuthState = (type: string, payload: IAuth) => {
 		return authDispatch({ type, payload });
 	};
