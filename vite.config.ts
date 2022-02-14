7e7bc8e73ae6bab3647a8a50173a19110d4dfaf0
Pedro Mendes <pedro.goncalves@bairesdev.com>
2022-02-14 11:24:37 -0300

    updating route config

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,6 @@ import { defineConfig } from "vite";
 import { VitePWA as pwa } from "vite-plugin-pwa";
 import manifest from "./manifest.json";
 import react from "@vitejs/plugin-react";
-import { dependencies } from "./package.json";
-
-const reactDeps = Object.keys(dependencies).filter((key) => key === "react" || key.startsWith("react-"));
-const manualChunks = {
-	vendor: reactDeps,
-	...Object.keys(dependencies).reduce((chunks, name) => {
-		if (!reactDeps.includes(name)) {
-			chunks[name] = [name];
-		}
-		return chunks;
-	}, {}),
-};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -26,10 +14,4 @@ export default defineConfig({
 			manifest,
 		}),
 	],
-	build: {
-		sourcemap: true,
-		rollupOptions: {
-			output: { manualChunks },
-		},
-	},
 });
