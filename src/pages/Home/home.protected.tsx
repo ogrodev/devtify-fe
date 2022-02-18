d66a509ac232ddbd0886ee1a8691ff94f3873609
Pedro Mendes <pedro.goncalves@bairesdev.com>
2022-02-17 22:23:20 -0300

    updating homepage

--- src/pages/Home/home.protected.tsx
+++ src/pages/Home/home.protected.tsx
@@ -1,7 +1,12 @@
+import styles from "./home.module.sass";
+import HeroSection from "../../components/Hero/hero";
+import FeaturedProducts from "../../components/Products/featuredProducts";
+
 export default function Home() {
 	return (
-		<div className="defaultPadding">
-			<h1>Home</h1>
+		<div className={`position-relative text-start defaultPadding pt-5 ${styles.container}`}>
+			<HeroSection />
+			<FeaturedProducts />
 		</div>
 	);
 }
