2ec6db24e7bce6ee66305d0dc12c5e2dec378180
Pedro Mendes <pedro.goncalves@bairesdev.com>
2022-02-22 01:30:02 -0300

    hardcoding workshop highlighted id

--- src/hooks/useAppMiddleware.ts
+++ src/hooks/useAppMiddleware.ts
@@ -1,4 +1,6 @@
+import { AxiosResponse } from "axios";
 import { Dispatch } from "react";
+import { IWorkshop } from "../interfaces/app.interface";
 import { ADD_WORKSHOP, UPDATE_APP, UPDATE_HIGHLIGHTS } from "../reducers/app.reducer";
 import { marketplaceService } from "../services/marketplace";
 import { workshopService } from "../services/workshop";
@@ -17,8 +19,11 @@ export default function useAppMiddleware() {
 	};
 
 	const fetchHighlightWorkshop = (appDispatch: Dispatch<any>) => {
-		workshopService.getHighlights().then((response) => {
-			appDispatch({ type: UPDATE_HIGHLIGHTS, payload: { workshop: response.data![0] } });
+		workshopService.getHighlights().then((response: AxiosResponse<IWorkshop[]>) => {
+			appDispatch({
+				type: UPDATE_HIGHLIGHTS,
+				payload: { workshop: response.data.find((ws) => ws.id === 21) },
+			});
 		});
 	};
 
